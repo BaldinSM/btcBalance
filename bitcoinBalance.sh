33d0b5ffb3dc16ac28a86ad1a2f8e226d185b002
Simon Baldin <simonmb92@gmail.com>
2022-12-04 15:01:11 +0600

    USD balance

--- bitcoinBalance.sh
+++ bitcoinBalance.sh
@@ -3,14 +3,22 @@
 while true
 do
   totalBalance=0
+
   while read p; do
     wget https://www.blockchain.com/btc/address/"$p" -q -O - >> "$p".html
     balance=$(awk '{for(i=1;i<=NF;i++)if($i~/address/ && $(i+1)~/is/ && $(i+3)~/BTC/){print $(i+2); exit}}' "$p".html)
     rm $p.html
-    echo "$p $balance BTC"
+    echo "$p $(printf "%.8f" $balance) BTC"
     totalBalance=$(bc <<< "$totalBalance + $balance")
-    sleep 1
+    sleep 0.5
   done < addresses
-  echo "Total balance of all addresses is $totalBalance BTC"
+
+  wget https://bitcoinwisdom.io/ -q -O - >> mainPage.html
+  btcPrice=$(awk 'BEGIN { FS="\""}{for(i=1;i<=NF;i++)if($i~/BTC\\\/USD/ && $(i+2)~/last_price/){print $(i+4)}}' mainPage.html)
+  totalBalanceUSD=$(bc <<< "$totalBalance * $btcPrice")
+  rm mainPage.html
+  echo "Total balance of all addresses is $(printf "%.8f" $totalBalance) BTC or $(printf "%.1f" $totalBalanceUSD) $"
+  echo
   sleep 3600
+
 done
